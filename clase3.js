eb22aa876f410345957105aa19ee0a4705df29dc
carloseduardo22 <carloseduardops22@gmail.com>
2017-06-03 12:58:58 -0400

    add html add js

---
+++ clase3.js
@@ -0,0 +1,24 @@
+var alto = 130;
+var ancho = 200;
+var colorRect;
+
+function setup(){
+  createCanvas(800,500);
+  background(255,150,0,200);
+}
+
+function draw(){
+  if(mouseX > width/2 - ancho/2 &&
+	mouseX < width/2 + ancho/2 &&
+	mouseY < height/2 + alto/2 &&
+	mouseY > height/2 - alto/2 ){
+    colorRect = color(0);
+  }
+  else {
+    colorRect = color(250,100,280);
+  }
+  noStroke();
+  fill(colorRect);
+  rectMode(CENTER);
+  rect(width/2,height/2,ancho,alto);
+}
